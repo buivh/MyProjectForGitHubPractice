2f0b89b7bfd5faa5b13396c58cd7b120d467bd7d
Denise Huynh <huynhd2@mail.uc.edu>
2024-04-02 12:06:59 -0400

    add new jhska

---
+++ MyProjectForGitHubPractice/Tuesday.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyProjectForGitHubPractice
+{
+    public class Tuesday
+    {
+        public Tuesday()
+        {
+            Console.WriteLine("Hi ")
+        }
+    }
+}
